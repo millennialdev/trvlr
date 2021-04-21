1d01204e28688f90944042a990132ea8d0bb8fe8
Spenser Wellen <spenserwellen@gmail.com>
2021-04-21 14:13:20 -0500

    fixed the upper/lower case folder structrue

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Nav from "../components/nav"
+import Nav from "../components/Nav"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Link } from "gatsby"
